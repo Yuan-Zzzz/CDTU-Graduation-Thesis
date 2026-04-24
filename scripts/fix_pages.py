c281bdbc95881ea05f6f37a3f26ec4912e550a25
Yuan <1729726526@qq.com>
2026-04-24 11:32:33 +0800

    添加页眉

--- scripts/fix_pages.py
+++ scripts/fix_pages.py
@@ -3,7 +3,7 @@ from docx.oxml import OxmlElement, parse_xml
 from docx.oxml.ns import qn
 from docx.opc.constants import RELATIONSHIP_TYPE as RT
 from docx.opc.packuri import PackURI
-from docx.parts.hdrftr import FooterPart
+from docx.parts.hdrftr import FooterPart, HeaderPart
 import copy
 from pathlib import Path
 
@@ -23,7 +23,6 @@ def insert_page_break_before(element):
     pb_p.append(pb_r)
     element.addprevious(pb_p)
 
-# 1. Find the sdt element (TOC) and insert a page break before it
 sdt_element = None
 for child in body:
     if child.tag.endswith('}sdt'):
@@ -58,23 +57,19 @@ for p in doc.paragraphs:
         break
 
 if chap1_p is not None:
-    # Create a new paragraph for the section break
     sect_p = OxmlElement('w:p')
     pPr = OxmlElement('w:pPr')
     sect_p.append(pPr)
     
-    # Copy the document's main sectPr
     doc_sectPr = body.sectPr
     new_sectPr = copy.deepcopy(doc_sectPr)
     
-    # Modify it to be a next page section break
     type_el = new_sectPr.find(qn('w:type'))
     if type_el is None:
         type_el = OxmlElement('w:type')
         new_sectPr.append(type_el)
     type_el.set(qn('w:val'), 'nextPage')
     
-    # Set Roman numerals for the front matter (this section)
     pgNumType_front = new_sectPr.find(qn('w:pgNumType'))
     if pgNumType_front is None:
         pgNumType_front = OxmlElement('w:pgNumType')
@@ -83,11 +78,8 @@ if chap1_p is not None:
     pgNumType_front.set(qn('w:start'), '1')
     
     pPr.append(new_sectPr)
-    
-    # Insert before Chapter 1
     chap1_p.addprevious(sect_p)
     
-    # Update the document's main sectPr (main matter) to restart page numbering at 1 with decimal format
     pgNumType_main = doc_sectPr.find(qn('w:pgNumType'))
     if pgNumType_main is None:
         pgNumType_main = OxmlElement('w:pgNumType')
@@ -95,15 +87,30 @@ if chap1_p is not None:
     pgNumType_main.set(qn('w:fmt'), 'decimal')
     pgNumType_main.set(qn('w:start'), '1')
 
-# 3. Modify the footer for the main body (Section 1 onwards) to display "第xx页"
+header_xml_str = '''<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<w:hdr xmlns:wpc="http://schemas.microsoft.com/office/word/2010/wordprocessingCanvas" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:r="http://schemas.openxmlformats.org/officeDocument/2006/relationships" xmlns:m="http://schemas.openxmlformats.org/officeDocument/2006/math" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:wp14="http://schemas.microsoft.com/office/word/2010/wordprocessingDrawing" xmlns:wp="http://schemas.openxmlformats.org/drawingml/2006/wordprocessingDrawing" xmlns:w10="urn:schemas-microsoft-com:office:word" xmlns:w="http://schemas.openxmlformats.org/wordprocessingml/2006/main" xmlns:w14="http://schemas.microsoft.com/office/word/2010/wordml" xmlns:w15="http://schemas.microsoft.com/office/word/2012/wordml" xmlns:wpg="http://schemas.microsoft.com/office/word/2010/wordprocessingGroup" xmlns:wpi="http://schemas.microsoft.com/office/word/2010/wordprocessingInk" xmlns:wne="http://schemas.microsoft.com/office/word/2006/wordml" xmlns:wps="http://schemas.microsoft.com/office/word/2010/wordprocessingShape" xmlns:wpsCustomData="http://www.wps.cn/officeDocument/2013/wpsCustomData" mc:Ignorable="w14 w15 wp14">
+  <w:p w14:paraId="46D6B860">
+    <w:pPr>
+      <w:pStyle w:val="77"/>
+      <w:rPr>
+        <w:color w:val="FF0000"/>
+      </w:rPr>
+    </w:pPr>
+    <w:r>
+      <w:t>成都工业学院本科毕业论文（设计）</w:t>
+    </w:r>
+  </w:p>
+</w:hdr>'''
+
+header_element = parse_xml(header_xml_str.encode('utf-8'))
+new_partname = doc.part.package.next_partname(PackURI('/word/header%d.xml'))
+new_header_part = HeaderPart(new_partname, 'application/vnd.openxmlformats-officedocument.wordprocessingml.header+xml', header_element, doc.part.package)
+header_rel_id = doc.part.relate_to(new_header_part, RT.HEADER)
+
 if len(doc.sections) > 1:
-    # We only need to create the new footer part once, then we can link it to all subsequent sections
     orig_footer_part = doc.sections[1].footer.part
-    
-    # Create a new footer XML by parsing the original one
     new_footer_xml = copy.deepcopy(orig_footer_part._element)
     
-    # Modify the new footer XML to add "第 " and " 页"
     begins = new_footer_xml.xpath('.//w:fldChar[@w:fldCharType="begin"]')
     ends = new_footer_xml.xpath('.//w:fldChar[@w:fldCharType="end"]')
     
@@ -117,21 +124,23 @@ if len(doc.sections) > 1:
         end_r = ends[0].getparent()
         end_r.addnext(r_suffix)
     
-    # Create a new FooterPart
-    new_partname = doc.part.package.next_partname(PackURI('/word/footer%d.xml'))
-    new_footer_part = FooterPart(new_partname, orig_footer_part.content_type, new_footer_xml, doc.part.package)
+    footer_partname = doc.part.package.next_partname(PackURI('/word/footer%d.xml'))
+    new_footer_part = FooterPart(footer_partname, orig_footer_part.content_type, new_footer_xml, doc.part.package)
+    footer_rel_id = doc.part.relate_to(new_footer_part, RT.FOOTER)
     
-    # Relate the document part to the new footer part
-    rel_id = doc.part.relate_to(new_footer_part, RT.FOOTER)
-    
-    # Update the sectPr for all sections starting from Section 1 to point to the new footer
     for i in range(1, len(doc.sections)):
         sectPr = doc.sections[i]._sectPr
-        # Find the existing footerReference
+        
+        header_ref = sectPr.find(qn('w:headerReference'))
+        if header_ref is None:
+            header_ref = OxmlElement('w:headerReference')
+            sectPr.insert(0, header_ref)
+        header_ref.set(qn('w:type'), 'default')
+        header_ref.set(qn('r:id'), header_rel_id)
+        
         footer_refs = sectPr.findall(qn('w:footerReference'))
         for ref in footer_refs:
-            # We only replace the default footer
             if ref.get(qn('w:type')) == 'default':
-                ref.set(qn('r:id'), rel_id)
+                ref.set(qn('r:id'), footer_rel_id)
 
 doc.save(str(FINAL_DOC))
